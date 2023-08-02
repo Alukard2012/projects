a6b6a281e9b72bb71995847660a033c0ae8cd2ad
Alukard2012 <101950344+Alukard2012@users.noreply.github.com>
2023-08-02 21:51:36 +0500

    Add Firefox support

--- search_scroll/search_scroll.js
+++ search_scroll/search_scroll.js
@@ -1,5 +1,22 @@
 'use strict'
-window.addEventListener("load", function (event) { //ждем загрузку стилей из CSS-файлов
-  const searchHighlight = document.getElementsByClassName('SearchHighlight'); //ищем стиль SearchHighlight
-  if (searchHighlight) searchHighlight.item(0).scrollIntoView({block: "center"}); //проматываем окно до первого выделенного стилем слова
-})
+if (navigator.userAgent.includes('Firefox')) //если используется Firefox, добавляем слушатель события DOMNodeInserted - оно устаревшее (удалить в случае проблем все до else)
+  window.addEventListener('DOMNodeInserted', scroll)
+else
+  window.addEventListener('load', scroll); //ждем загрузку стилей из CSS-файлов
+
+function scroll () {
+  const searchHighlight = document.querySelector('span.SearchHighlight'); //ищем стиль SearchHighlight
+  if (searchHighlight) searchHighlight.scrollIntoView({block: "center"}); //проматываем окно до первого выделенного стилем слова
+}
+
+
+//нужно доработать, пока не работает
+/* const target = document.querySelector('body');
+
+const observer = new MutationObserver(mutations => {
+  if (document.querySelector('span.SearchHighlight')) document.querySelector('span.SearchHighlight').scrollIntoView({block: "center"});
+});
+
+const config = { attributes: true, childList: true, characterData: true };
+
+observer.observe(target, config); */
